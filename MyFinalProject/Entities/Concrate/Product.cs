24085fd0380aff1229a1fe0a52780d4da57e21f2
Engin Karataş <enginkaratas99@gmail.com>
2021-02-10 18:32:14 +0300

    layers and entities setup

---
+++ MyFinalProject/Entities/Concrate/Product.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrate
+{
+    public class Product:IE ntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitInStock{ get; set; } //16 bit signed integer (intin bir küçüğü)
+        public decimal UnitPrice { get; set; }
+    }
+}

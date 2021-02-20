8c1631ec4c39d0d83a847910521603d2f49660d3
Engin Karataş <enginkaratas99@gmail.com>
2021-02-20 22:32:08 +0300

    refactoring part 2 : EfRepositoryBase.cs - core

---
+++ MyFinalProject/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+
+    }
+}

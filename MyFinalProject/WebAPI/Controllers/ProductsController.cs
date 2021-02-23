5af24ebda2bb6fd06c51016b5652daa5f7c125cb
Engin Karataş <enginkaratas99@gmail.com>
2021-02-23 17:58:56 +0300

    🎯WebAPI layer added simple request to localhost:xxx/api/products

---
+++ MyFinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Business.Concrate;
+using Core.Utilities.Results;
+using DataAccess.Concrate.EntityFramework;
+using Entities.Concrate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {   
+        [HttpGet]                                                           
+        public List<Product> Get()
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+        }
+    }
+}
